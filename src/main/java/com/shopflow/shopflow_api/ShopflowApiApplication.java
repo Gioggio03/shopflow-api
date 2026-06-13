902247e7f457cf25447b985f248f41a3667e63af
Gioggio03 <g.cappello12@studenti.unipi.it>
2026-06-13 20:02:53 +0200

    chore: initial project setup (Phase 0)

---
+++ src/main/java/com/shopflow/shopflow_api/ShopflowApiApplication.java
@@ -0,0 +1,13 @@
+package com.shopflow.shopflow_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopflowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopflowApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopflow/shopflow_api/ShopflowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopflow.shopflow_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopflowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
